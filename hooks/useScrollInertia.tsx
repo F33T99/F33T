910a9304c5b52c9d3e13d29a06043c932efc03ab
d91 <dominik.tomcik23@gmail.com>
2023-07-20 14:58:21 +0200

    reduced acceleration

--- hooks/useScrollInertia.tsx
+++ hooks/useScrollInertia.tsx
@@ -19,7 +19,7 @@ export function useScrollInertia(ref) {
   });
 
   const handleWheel = (e: WheelEvent) => {
-    speed.current += e.deltaY * 0.8;
+    speed.current += e.deltaY * 0.4;
   };
 
   React.useEffect(() => {
