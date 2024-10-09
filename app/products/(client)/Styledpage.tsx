19f7811cd5e23f7f20810ba6ec3833d36f82b566
d91 <dominik.tomcik23@gmail.com>
2024-10-09 20:03:33 +0200

    styles(products): tweaks

--- app/products/(client)/Styledpage.tsx
+++ app/products/(client)/Styledpage.tsx
@@ -17,6 +17,7 @@ export const StyledProductPage = styled.div`
   padding: ${spaces.xxxxl}px ${spaces.l}px 0;
   ${breakpoint.phone} {
     padding: ${spaces.xxxl}px ${spaces.m}px 0;
+    row-gap: ${spaces.xl}px;
   }
   ${breakpoint.smallPhone} {
     padding: ${spaces.xxl}px ${spaces.s}px 0;
@@ -43,14 +44,12 @@ export const ProductsGrid = styled.div`
   display: grid;
   row-gap: ${spaces.xxxl}px;
   ${breakpoint.phone} {
-    margin: ${spaces.xxl}px 0;
+    margin: 0 0 ${spaces.xxl}px;
     row-gap: ${spaces.xxl}px;
   }
   ${breakpoint.smallPhone} {
-    margin-top: ${spaces.xl}px;
     row-gap: ${spaces.xl}px;
   }
   ${breakpoint.monitor} {
-    margin-top: ${spaces.xxxxl}px;
   }
 `;
